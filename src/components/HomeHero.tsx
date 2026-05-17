6502e987d51bbcc100e173859cbb6dab296fdfd9
Kashif Ali <alyyk82@gmail.com>
2026-05-17 16:19:12 +0500

    updated

--- src/components/HomeHero.tsx
+++ src/components/HomeHero.tsx
@@ -4,27 +4,26 @@ import { ButtonLink } from "./ButtonLink";
 export function HomeHero() {
   return (
     <section className="relative overflow-hidden text-white">
-      {/* Background Image */}
-      <Image
-        src="/images/Hero-bg.webp"
-        alt="Online Quran Academy"
-        fill
-        priority
-        fetchPriority="high"
-        quality={100}
-        sizes="100vw"
-        className="object-cover object-center -z-20 sm:block hidden"
-      />
+      <div className="hidden md:block">
+        <Image
+          src="/images/Hero-bg.webp"
+          alt="Online Quran Academy"
+          fill
+          priority
+          fetchPriority="high"
+          quality={70}
+          sizes="100vw"
+          className="object-cover object-center -z-20"
+        />
+      </div>
 
-      {/* Overlay */}
-      <div className="absolute sm:hidden inset-0 -z-10">
-        <div className="absolute inset-0 bg-linear-to-r from-(--emerald-deep)  to-(--emerald-deep)" />
+      <div className="absolute md:hidden inset-0 -z-10">
+        <div className="absolute inset-0 bg-linear-to-r from-(--emerald-deep) to-(--emerald-deep)" />
         <div className="absolute inset-0 bg-[radial-gradient(circle_at_75%_22%,rgba(230,169,54,0.22),transparent_18rem)]" />
       </div>
 
-      {/* Content */}
-      <div className="container-page relative grid h-auto sm:min-h-190 items-center gap-10 pb-16 pt-44 sm:pt-40 lg:min-h-195 lg:pb-0 lg:pt-32 md:grid-cols-[1fr_0.86fr]">
-        <div className="md:max-w-2xl w-full">
+      <div className="container-page relative grid h-auto md:min-h-190 items-center gap-10 pb-16 pt-44 sm:pt-40 lg:min-h-195 lg:pb-0 lg:pt-32 md:grid-cols-[1fr_0.86fr]">
+           <div className="md:max-w-2xl w-full">
           <p className="mb-3 w-fit rounded-full border border-white/14 bg-white/10 px-4 py-2 text-xs font-extrabold uppercase tracking-[0.2em] text-(--gold-soft) backdrop-blur-md sm:text-sm">
             Online Quran Academy
           </p>
@@ -48,16 +47,13 @@ export function HomeHero() {
           </div>
         </div>
 
-        {/* Right Hero Image */}
         <div className="relative hidden min-h-107.5 overflow-hidden rounded-4xl border border-white/18 bg-white/10 p-3 shadow-[0_24px_80px_rgba(0,0,0,0.28)] backdrop-blur md:block">
           <Image
             src="/images/heroImg.webp"
             alt="Student learning Quran online"
             fill
-            priority
-            fetchPriority="high"
-            quality={100}
-            sizes="(min-width: 1024px) 420px, 100vw"
+            quality={75}
+            sizes="(max-width:768px) 0px, (max-width:1024px) 40vw, 420px"
             className="object-cover p-3"
           />
         </div>
